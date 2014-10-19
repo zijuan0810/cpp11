71f07d6b6f781df96a8405174119937e8f3595da
zijuan0810 <zijuan0810@163.com>
2014-10-20 00:27:02 +0800

    add virtual_inheritance

---
+++ virtual_inheritance/virtual_inheritance.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+class Animal {
+public:
+	virtual void eat() { cout << __FUNCTION__ << endl; }
+};
+
+// Two classes virtually inheriting Animal:
+class Mammal : public virtual Animal {
+public:
+	virtual void breathe() { cout << __FUNCTION__ << endl; }
+};
+
+class WingedAnimal : public virtual Animal {
+public:
+	virtual void flap() { cout << __FUNCTION__ << endl; }
+};
+
+// A bat is still a winged mammal
+class Bat : public Mammal, public WingedAnimal {
+};
+
+int main()
+{
+	Bat b;
+	b.eat();
+
+	Animal& a = b;
+	a.eat();
+
+	return 0;
+}
